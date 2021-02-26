28c361985b24436529b8165a18713467daa0b4f1
Marcus Vinicius de Carvalho Botelho <marcus.botelho@dxtcps004203.local>
2021-02-25 21:41:13 -0300

    Add notification web and notification sound

--- src/context/ChallengerContext.tsx
+++ src/context/ChallengerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import challenges from "../../challenges.json";
 
 interface Challenge {
@@ -33,6 +33,10 @@ export function ChallengerProvider({ children }: ChallengerProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   const levelUp = () => setLevel(level + 1);
 
   const startNewChallenge = () => {
@@ -40,6 +44,14 @@ export function ChallengerProvider({ children }: ChallengerProviderProps) {
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge);
+
+    new Audio("/notification.mp3").play();
+
+    if (Notification.permission === "granted") {
+      new Notification("Novo desafio", {
+        body: `Valendo ${challenge.amount}XP`,
+      });
+    }
   };
 
   const resetChallenge = () => {
